c79155a75e7baeec0e9b9132d61f1da1afaa552d
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-29 22:20:26 +0300

    Create Point3D.js

---
+++ hw1/ModelElements /Point3D.js
@@ -0,0 +1,31 @@
+class Point3D {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  getX() {
+    return this.x;
+  }
+
+  setX(x) {
+    this.x = x;
+  }
+
+  getY() {
+    return this.y;
+  }
+
+  setY(y) {
+    this.y = y;
+  }
+
+  getZ() {
+    return this.z;
+  }
+
+  setZ(z) {
+    this.z = z;
+  }
+}
