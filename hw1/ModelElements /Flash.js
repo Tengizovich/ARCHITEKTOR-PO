426a44f63730ee518a698ea5dbb689d009832440
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-29 22:16:17 +0300

    Create Flash.js

---
+++ hw1/ModelElements /Flash.js
@@ -0,0 +1,18 @@
+class Flash {
+  constructor(location, angle, color, power) {
+    this.location = location;
+    this.angle = angle;
+    this.color = color;
+    this.power = power;
+  }
+
+  rotate(grad) {
+    // Реализация метода для вращения
+    // использовать переданный угол и изменить угол молнии
+  }
+
+  move(cm) {
+    // Реализация метода для перемещения
+    // использовать переданные данные расстояния и изменить положение молнии
+  }
+}
