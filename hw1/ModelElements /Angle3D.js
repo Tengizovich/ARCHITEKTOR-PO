451ae4b0b799562ca9a7f0c5fb73e9135a688ddb
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-29 22:06:04 +0300

    Create Angle3D.js

---
+++ hw1/ModelElements /Angle3D.js
@@ -0,0 +1,31 @@
+class Angle3D {
+  constructor() {
+    this.angleX = 0;
+    this.angleY = 0;
+    this.angleZ = 0;
+  }
+
+  getAngleX() {
+    return this.angleX;
+  }
+
+  setAngleX(angleX) {
+    this.angleX = angleX;
+  }
+
+  getAngleY() {
+    return this.angleY;
+  }
+
+  setAngleY(angleY) {
+    this.angleY = angleY;
+  }
+
+  getAngleZ() {
+    return this.angleZ;
+  }
+
+  setAngleZ(angleZ) {
+    this.angleZ = angleZ;
+  }
+}
