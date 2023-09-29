9ac51448a3c652272d409ae342c467ede8d846e4
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-29 22:29:50 +0300

    Create Texture.js

---
+++ hw1/ModelElements /Texture.js
@@ -0,0 +1,11 @@
+class Texture {
+  constructor() {
+    // Конструктор класса
+  }
+
+  // Добавьте методы и свойства класса здесь
+}
+
+// Использование класса Texture
+const texture = new Texture();
+// Создание экземпляра класса Texture
