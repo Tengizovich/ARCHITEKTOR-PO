7df267f10e0a9b8b2dcc3ee65773838cde2336ae
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-29 22:08:05 +0300

    Create Camera.js

---
+++ hw1/ModelElements /Camera.js
@@ -0,0 +1,14 @@
+class Camera {
+  constructor() {
+    this.location = new Point3D();
+    this.angle = new Angle3D();
+  }
+
+  rotate(grad) {
+    // Реализуйте поворот камеры на заданный угол
+  }
+
+  move(cm) {
+    // Реализуйте перемещение камеры на заданное расстояние
+  }
+}
