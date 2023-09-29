7a2de70d0fc441ef438ec1b9c9ec2d5dbcbe4b87
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-29 22:27:47 +0300

    Create Scene.js

---
+++ hw1/ModelElements /Scene.js
@@ -0,0 +1,16 @@
+class Scene {
+  constructor() {
+    this.id = 0;
+    this.models = [];
+    this.flashes = [];
+  }
+
+  method1(in1) {
+    return in1;
+  }
+
+  method2(in1, in2) {
+    let ret = new Object();
+    return ret;
+  }
+}
